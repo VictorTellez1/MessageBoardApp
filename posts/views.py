9bd152774fd4cecd50343a5086e623d576b0f3a8
VictorTellez1 <victortellez691@gmail.com>
2024-02-20 20:46:26 -0600

    Added tests

--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,5 @@ class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list' #ahora lo podemos usar en lugar de context-object-name
+
+
